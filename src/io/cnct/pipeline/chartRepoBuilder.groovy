8a453af99a980d1469ee935501dc00f244be10b8
Marat Garafutdinov <maratoid@gmail.com>
2019-02-21 10:00:48 -0800

    Update chartRepoBuilder.groovy

--- src/io/cnct/pipeline/chartRepoBuilder.groovy
+++ src/io/cnct/pipeline/chartRepoBuilder.groovy
@@ -498,7 +498,7 @@ def buildsTestHandler(scmVars) {
     }
 
     stage('Collect cve scan targets') {
-      def clairService = "clair:6060"
+      def clairService = "clair-clair:6060"
       def maxCve = pipeline.cveScan.maxCve
       def maxLevel = pipeline.cveScan.maxLevel
       def ignoreCVE = pipeline.cveScan.ignore
